3dfef40ebacd0062887214fd08dcd8f317301bb7
Filipe Barros <tiagosoarez6@gmail.com>
2023-04-26 16:58:14 +0100

    criacao da view e controller para login e registro

--- src/main/java/org/example/controllers/LoginController.java
+++ src/main/java/org/example/controllers/LoginController.java
@@ -1,14 +1,11 @@
 package org.example.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnLogin;
 }
--- src/main/java/org/example/controllers/RegistroController.java
+++ src/main/java/org/example/controllers/RegistroController.java
@@ -1,4 +1,21 @@
 package org.example.controllers;
 
-public class RegistroController {
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistroController implements Initializable {
+
+    @FXML
+    private Button btnRegistrar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        /*
+        Aqui coloca-se tipicamente açoes que quero que sejam executadas quando instancio o controlador
+         */
+    }
 }
