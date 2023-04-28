68387fe17e8f0d195ed020e93f4e99e25a458923
Filipe Barros <tiagosoarez6@gmail.com>
2023-04-28 16:05:44 +0100

    view e controller login e registro completo dos e id e actions

--- src/main/java/org/example/controllers/LoginController.java
+++ src/main/java/org/example/controllers/LoginController.java
@@ -1,11 +1,42 @@
 package org.example.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     private Button btnLogin;
+
+    @FXML
+    private CheckBox checkBoxLoginGuardaPass;
+
+    @FXML
+    private Hyperlink hyperlinkLogin;
+
+    @FXML
+    private Label labelErroLoginNomeEmail;
+
+    @FXML
+    private Label labelErroLoginPass;
+
+    @FXML
+    private TextField labelLoginNomeEmail;
+
+    @FXML
+    private TextField labelLoginPass;
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/org/example/controllers/RegistroController.java
+++ src/main/java/org/example/controllers/RegistroController.java
@@ -1,8 +1,12 @@
 package org.example.controllers;
 
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +15,38 @@ public class RegistroController implements Initializable {
 
     @FXML
     private Button btnRegistrar;
+
+    @FXML
+    private Hyperlink hyperlinkLogin;
+
+    @FXML
+    private Label labelErroRegistroConfirmaPass;
+
+    @FXML
+    private Label labelErroRegistroEmail;
+
+    @FXML
+    private Label labelErroRegistroNome;
+
+    @FXML
+    private Label labelErroRegistroPass;
+
+    @FXML
+    private TextField labelRegistroConfirmaPass;
+
+    @FXML
+    private TextField labelRegistroEmail;
+
+    @FXML
+    private TextField labelRegistroNome;
+
+    @FXML
+    private TextField labelRegistroPass;
+
+    @FXML
+    void btnRegistrar(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
